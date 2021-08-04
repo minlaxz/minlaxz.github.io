0c0e0f847db8c49bbd373883adeb96ec42d4b3a9
Min Latt <minminlaxz@gmail.com>
2021-08-04 21:13:29 +0630

    JavaScript Closures

---
+++ src/Clock/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const TikTik = () => {
+    const [currentTime, setCurrentTime] = React.useState(new Date());
+
+    // javascript closures
+    const aSingleTick = React.useCallback(() => {
+        setCurrentTime(new Date());
+    })
+
+    React.useEffect(()=>{
+        // on component mount
+        let timerId = setInterval(() => aSingleTick(), 1000);
+        // component unmount
+        return () => clearInterval(timerId);
+            
+    },[aSingleTick]);
+
+    return (
+        <span>
+            {currentTime.toLocaleTimeString()}
+        </span>
+    );
+}
+
+export default TikTik;
