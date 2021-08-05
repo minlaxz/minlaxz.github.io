1f236bbaa941499efa4a4b12164a744e6f133d24
Min Latt <minminlaxz@gmail.com>
2021-08-05 23:51:53 +0630

    Fetch repos

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -3,11 +3,30 @@ import styles from './Home.module.css';
 import { ToRepos, ToTech } from '../routes';
 import TikTik from '../Clock';
 import { Alink } from '../Units';
+import produce from 'immer';
 // A simple home component
 const Home = () => {
     React.useEffect(() => {
         window.document.title = 'minlaxz | Home'
+
+        const domain = window.location.hostname;
+        // const domain = 'minlaxz.github.io'
+        const minlaxzData = localStorage.getItem(domain);
+
+        if (minlaxzData) { // visted user
+            const newState = produce(JSON.parse(minlaxzData), (draft) => {
+                draft.lastVisitedTime = Date.now()
+            })
+            localStorage.setItem('minlaxz.github.io', JSON.stringify(newState));
+        } else { // new user
+            const data = {
+                lastVisitedTime: Date.now()
+            }
+            localStorage.setItem('minlaxz.github.io', JSON.stringify(data));
+        }
+
     }, []);
+
     return (
         <>
             <div className={`${styles.root_container}`}>
@@ -16,9 +35,9 @@ const Home = () => {
                 <p>Source Code is hosted <Alink to="https://github.com/minlaxz/minlaxz.github.io" text="here" /></p>
                 <p>View<ToRepos linkClass={`${styles.to_repos}`} /> <span><small>yep it is uesEffect</small></span></p>
                 <ul>
-                This page is also avaiable at...
-                    <li><Alink to="https://minlaxz.github.io" text="on Github Pages"/></li>
-                    <li><Alink to="https://gh-minlaxz.pages.dev" text="on Cloudflare Pages (not yet)"/></li>
+                    This page is also avaiable at...
+                    <li><Alink to="https://minlaxz.github.io" text="on Github Pages" /></li>
+                    <li><Alink to="https://gh-minlaxz.pages.dev" text="on Cloudflare Pages (not yet)" /></li>
                 </ul>
                 <small style={{ width: "80%", textAlign: "center", padding: "10px", fontSize: "14px" }}>
                     Reactjs with Vite build tool, hosted on github pages with actions, SSL/TLS by Cloudflare with proxies.
