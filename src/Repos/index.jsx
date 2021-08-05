1867d396954efd7c6248882a89472afc864ad2e2
Min Latt <minminlaxz@gmail.com>
2021-08-05 16:44:44 +0630

    Update Repo Component

--- src/Repos/index.jsx
+++ src/Repos/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import styles from './Repos.module.css';
 import { ToHome } from '../routes';
+
+const Repo = () => {
+    return (
+        <>
+        </>
+    )
+}
 
 const Repos = () => {
     React.useEffect(() => {
         window.document.title = 'minlaxz | Repos';
     }, []);
     return (
-        <>
+        <div className={`${styles.root}`}>
             This is repos route.
             <ToHome />
-        </>
+        </div>
     );
 }
 
