df82dd62490232e055ba64d1e5c6d4ea0b822e57
Min Latt <minminlaxz@gmail.com>
2021-08-06 12:50:16 +0630

    Using styled components

--- src/Tech/index.jsx
+++ src/Tech/index.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
-import styles from './Tech.module.css';
-import { Alink } from '../Units';
+// import styles from './Tech.module.css';
+// import { Alink } from '../Units';
 import { ToHome } from '../routes';
+import styled from 'styled-components';
+
+const Box = styled.div`
+margin:0;
+padding:0;
+display: flex;
+flex-flow: column wrap;
+align-items: center;
+justify-content: center;
+height: 100vh;
+`;
+
+const Pre = styled.pre`
+white-space: pre-wrap;
+font-size: 0.9rem;
+min-width: 0;
+`;
+
+const Code = styled.code`
+color: red;
+`;
 
 const TechList = () => {
     React.useEffect(() => {
         window.document.title = 'minlaxz | About';
     }, []);
+
     return (
-        <div>
-            <ul className={`${styles.ulstyles}`}>
-                <li><Alink to="https://reactjs.org" text="React" /></li>
-                <pre>Javascript Frontend Framework</pre>
-                <li><Alink to="https://github.com/minlaxz/minlaxz.github.io/blob/ac1740f95bceac92041a74184bb2d928755e91a5/CNAME#L1" text="CNAME" /></li>
-                <pre>pointing to minlaxz.github.io</pre>
-                <li><Alink to="https://github.com/minlaxz/minlaxz.github.io/blob/main/.github/workflows/deploy-pages-on-pr-to-main.yml" text="CI (github actions)" /></li>
-                <pre>Continuos Integration and Deployment</pre>
-                <li><Alink to="https://github.com/css-modules/css-modules" text="CSS Modules" /></li>
-                <pre>CSS as Modules</pre>
-                <li><Alink to="https://github.com/vitejs/vite" text="Vite is fast" /></li>
-                <pre>Vite as build tool</pre>
-            </ul>
+        <Box>
+            <Pre>
+                Hello, world!
+                in this page I've used styled components and <Code>pre</Code>
+            </Pre>
             <ToHome />
-        </div>
+        </Box>
+
     );
 }
 
