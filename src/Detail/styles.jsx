cf0a7f804aac90bcefc0802176eaea6918f6b785
Min Latt <minminlaxz@gmail.com>
2021-08-08 21:36:44 +0630

    Code split for styled

---
+++ src/Detail/styles.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const MainView = styled.div`
+display: flex;
+flex-flow: column;
+flex-wrap: wrap;
+justify-content: center;
+align-items: center;
+height: 90vh;
+background-color: #fff;
+`;
+
+export const PreTag = styled.pre`
+white-space: pre-wrap;
+/* word-break: break-all; */
+text-align: center;
+`;
+
+
