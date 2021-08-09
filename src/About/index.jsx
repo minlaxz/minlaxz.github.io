7e9d73bcbad947592e8befb0186e10bc07dbcacc
minlaxz <minminlaxz@gmail.com>
2021-08-09 20:13:17 +0630

    About page

--- src/About/index.jsx
+++ src/About/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { ToHome } from '../routes';
-import { Box, Pre, Code } from './styles';
+import { Container, Pre, Code, DivUl, UlView, LiView } from './styles';
+
+const Li = ({ tech }) => <LiView>{tech}</LiView>;
+
+const UlwithDiv = ({ children, head }) => <DivUl>{head}<UlView>{children}</UlView></DivUl>;
 
 const About = () => {
     React.useEffect(() => {
@@ -8,13 +12,29 @@ const About = () => {
     }, []);
 
     return (
-        <Box>
+        <Container>
+            <UlwithDiv head="I've used the following tools">
+                <Li tech="React for fontend" />
+                <Li tech="styled componenet for CSS" />
+                <Li tech="Vite as build tool" />
+                <Li tech="React hash router for frontend routing" />
+            </UlwithDiv>
+            <UlwithDiv head="I've used these for domain">
+                <Li tech="This is hosted on github pages" />
+                <Li tech="This is also hosted on cloudflare pages" />
+                <Li tech="used CNAME for domain routing" />
+            </UlwithDiv>
+            <UlwithDiv head="What is cool ..?">
+                <Li tech="Clock is about Javascript Closures" />
+                <Li tech="Repo page is about axios with useEffect()" />
+                <Li tech="Markdown page is about map() and filter()" />
+                <Li tech="gh-pages package is cool but this used Github actions workflow" />
+            </UlwithDiv>
             <Pre>
-                Hello, world!
-                in this page I've used styled components and <Code>pre</Code>
+                <Code>I will made a backend for this 🤪</Code>
             </Pre>
-            <ToHome />
-        </Box>
+            <ToHome cusName="Back to Home" />
+        </Container>
 
     );
 }
--- src/About/styles.jsx
+++ src/About/styles.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Box = styled.div`
+export const Container = styled.div`
 margin:0;
 padding:0;
 display: flex;
@@ -12,10 +12,31 @@ height: 100vh;
 
 export const Pre = styled.pre`
 white-space: pre-wrap;
-font-size: 0.9rem;
+font-size: 0.8rem;
 min-width: 0;
 `;
 
 export const Code = styled.code`
 color: red;
+`;
+
+export const UlView = styled.ul`
+list-style-type: lower-greek;
+`;
+
+export const LiView = styled.li`
+font-size: 18px;
+margin: 0.4em;
+padding: 0.4em;
+@media only screen and (max-width: 600px) {
+font-size: 15px;
+margin: 0.2em;
+padding: 0.2em;
+}
+
+`;
+
+export const DivUl = styled.div`
+margin: 0.5em;
+padding: 0.5em;
 `;
