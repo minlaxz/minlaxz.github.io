9ed362060aa17135d83371aeeccb8df4e7dac7cc
minlaxz <minminlaxz@gmail.com>
2021-08-12 20:42:14 +0630

    JSDoc

--- src/Serverless/index.jsx
+++ src/Serverless/index.jsx
@@ -3,9 +3,16 @@ import { ToHome, ToRepos, ToHuman } from '../routes';
 import { Container } from './styles';
 
 const ServerlessApi = () => {
+    /** fetch using axios to serverless api
+     * @param {string} query The query to be searched on `unsplash`
+     * cuz this is a frontend app, i need to deal with CORS headers
+    */
+
+
     return (
         <Container>
             <h3>You're gonna make serverless api request 👻</h3>
+            {/* Render images in responsive size */}
             <div style={{ display: 'flex', width: "100vw", flexDirection: 'row', justifyContent: 'space-evenly', marginBottom: '1em' }}>
                 <ToHome cusName="Return Home 😭" />
                 <ToHuman cusName="To markdown 🥶" />
