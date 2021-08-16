0f80e201c5a6dde5e748557d7d7816b61d4c83b9
minlaxz <minminlaxz@gmail.com>
2021-08-16 23:58:43 +0630

    Backend endpoint updated

--- src/Components/SVC/index.jsx
+++ src/Components/SVC/index.jsx
@@ -5,6 +5,8 @@ const SourceVersion = () => {
 
     const [checksum, setChecksum] = React.useState("");
     const repo = 'minlaxz.github.io';
+    const user = 'minlaxz';
+    const branch = 'main';
 
     React.useEffect(() => {
         const endpoint = process.env.NODE_ENV === "development" ? "http://localhost:8787/api/github/lastcommit" : "https://lessapi.minlaxz.workers.dev/api/github/lastcommit"
@@ -12,9 +14,10 @@ const SourceVersion = () => {
             await axios.get(`${endpoint}`, {
                 headers: { 'Content-type': 'application/json' },
                 params: {
-                    repo: repo /* { repo } but want some verbose */
+                    repo, user, branch
+
                 }
-            }).then(response => setChecksum(response.data)).catch(err => console.log(err));
+            }).then(response => setChecksum(response.data.sha)).catch(err => console.log(err));
         };
         fetchSha();
     }, []);
