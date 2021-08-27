fbf05ee007a06555a85666286fed0a7c83f81d61
minlaxz <minminlaxz@gmail.com>
2021-08-27 23:13:30 +0630

    Some changes

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { NormalContainer } from '@/Components/Containers';
 import { Form, EmailInput, PasswordInput, LoginButton } from './styles';
 import { ToSignup } from '@/Routes/';
+import { ToHome } from '@/Routes/';
 
 const Login = () => {
     return (
         <NormalContainer>
             <Form onSubmit={() => alert(0)}>
+                <ToHome cusName="Go HOME 🏡"/>
                 <div>
-                    <label htmlFor="exampleInputEmail1">Email address : &nbsp;</label>
+                    {/* <label htmlFor="exampleInputEmail1">Email address : &nbsp;</label> */}
                     <EmailInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                     {/* <small id="emailHelp">We'll never share your email with anyone else.</small> */}
                 </div>
                 <div>
-                    <label htmlFor="exampleInputPassword1">Password : &nbsp;</label>
+                    {/* <label htmlFor="exampleInputPassword1">Password : &nbsp;</label> */}
                     <PasswordInput type="password" id="exampleInputPassword1" aria-describedby="passwordHelp" placeholder="Enter Password" />
                 </div>
                 <div>
--- src/Pages/Signup/styles.jsx
+++ src/Pages/Signup/styles.jsx
@@ -5,8 +5,8 @@ export const Form = styled.form`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     padding: 0px 20px;
     margin: 0px;
     background-color: #f5f5f5;
@@ -42,6 +42,7 @@ export const PasswordInput = styled.input`
     margin: 10px 0px;
     border: 1px solid #ccc;
     border-radius: 5px;
+    color: darkred;
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 `;
 
