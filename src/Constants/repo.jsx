e67e66e68b1547873db6288946a789c789decf3e
minlaxz <minminlaxz@gmail.com>
2021-09-27 12:38:49 +0630

    Another Reducer for LastCommit

---
+++ src/Constants/repo.jsx
@@ -0,0 +1,4 @@
+export const repo = `minlaxz.github.io`;
+export const user = `minlaxz`;
+export const mainBranch = `main`;
+export const devBranch = `devel`;
--- src/Reducers/index.jsx
+++ src/Reducers/index.jsx
@@ -3,5 +3,6 @@ import points from './points';
 import user from './user';
 import authUser from './authUser';
 import theme from './theme';
+import lastCommit from './lastCommit';
 
-export default combineReducers({ pointss: points, user: user, authUser: authUser, theme });
+export default combineReducers({ pointss: points, user: user, authUser: authUser, theme, lastCommit });
