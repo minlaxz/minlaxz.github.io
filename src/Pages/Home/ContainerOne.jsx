f553e735dd471ba57423d284236102e287b6cbb5
minlaxz <minminlaxz@gmail.com>
2021-09-30 23:27:26 +0630

    Some update

--- src/Pages/Home/ContainerOne.jsx
+++ src/Pages/Home/ContainerOne.jsx
@@ -29,7 +29,7 @@ justify-content: center;
 
 export default () => (
     <ContainerOne>
-        <h3 className="animate__animated animate__flipInX animate__slower">Hello, world! ((👻) =&gt; (minlaxz)) </h3>
+        <h4 className="animate__animated animate__flipInX animate__slower">( minlaxz = (&nbsp;) =&gt; async dispatch =&gt; 👻 ) </h4>
         <code style={{ fontSize: "13px" }} className="animate__animated animate__flipInX">Welcome to my universe. <TikTik /></code>
         <Border>
             <Pre>You would visit to me with <Alink to="https://git.io/minlaxz" text="git.io/minlaxz"></Alink> if this url <u style={{ color: "hotpink" }}>{window.location.hostname}</u> is <b>long</b> to remember for you 🥴</Pre>
