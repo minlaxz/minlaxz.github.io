44a323efb1e19b047ab630f849b27aea34761816
minlaxz <minminlaxz@gmail.com>
2021-11-07 00:56:55 +0630

    Update layout

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
 import styles from '../styles/Home.module.css'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Min Min Latt | Portfolio</title>
-        <meta name="description" content="Min Min Latt, minlaxz portfolio site." />
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-
-      <div className='mainContainer'>
-        <h3>
-          Hello, world!
-        </h3>
+      <section className={utilStyles.headingMd}>
+        <p>I am a Software Developer ...</p>
+        <code style={{ fontSize: "14px" }}> Use to create stuffs 🚀  by reading documentations 🗒️ and fixed bugs 🐛 by following discussions and thoughts 💭 </code>
+        <br />
         <p>
-          My name is Min Min Latt, and I am a software developer.
+          <small>Here is my OpenPGP ID:
+            <a
+              href='https://keys.openpgp.org/search?q=minminlaxz%40gmail.com'
+              target={`_blank`}
+              rel="noopener noreferrer">
+              {' '}D047EEF6CCB71C5D&#8599;
+            </a>
+          </small>
         </p>
-        <code>OpenPGP ID:
-          <a
-            href='https://keys.openpgp.org/search?q=minminlaxz%40gmail.com'
-            rel='noopener noreferer'
-            target={`_blank`}
-          >D047EEF6CCB71C5D</a></code>
-        <style jsx>{`
-          .mainContainer {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100%;
-          }
-          `}</style>
-      </div>
+      </section>
 
 
       <footer className={styles.footer}>
@@ -42,12 +34,29 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/images/vercel-triangle-black.svg" alt="Vercel Logo" width={28} height={28} />
+          </span>
+        </a>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className={styles.logo}>
+            <Image src="/images/react-logo.svg" alt="ReactJS Logo" width={30} height={30} />
+          </span>
+        </a>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className={styles.logo}>
+            <Image src="/images/nextjs-black-logo.svg" alt="NextJS Logo" width={48} height={30} />
           </span>
         </a>
       </footer>
-    </div>
+    </Layout>
   )
 }
