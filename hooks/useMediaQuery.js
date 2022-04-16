391dcf7fe9c9d48fcc5c43ebac061151a98a0d7e
Min Min Latt <minminlaxz@gmail.com>
2022-04-16 11:57:04 +0630

    Update hooks

--- hooks/useMediaQuery.js
+++ hooks/useMediaQuery.js
@@ -16,4 +16,21 @@ const useMediaQuery = (query) => {
     return matches;
 }
 
+export const useOnScreen = (ref) => {
+
+    const [isIntersecting, setIntersecting] = useState(false)
+
+    const observer = new IntersectionObserver(
+        ([entry]) => setIntersecting(entry.isIntersecting)
+    )
+
+    useEffect(() => {
+        observer.observe(ref.current)
+        // Remove the observer as soon as the component is unmounted
+        return () => { observer.disconnect() }
+    }, [])
+
+    return isIntersecting
+}
+
 export default useMediaQuery;
