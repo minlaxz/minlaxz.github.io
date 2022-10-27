c32e74b656476d14ee90c9a1e5157e05189d2289
minlaxz <minminlaxz@gmail.com>
2022-10-27 22:40:30 +0630

    update: Animated toggler

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,3 +1,7 @@
 // animated toggler https://jfelix.info/blog/using-react-spring-to-animate-svg-icons-dark-mode-toggle
 // scrollbar https://css-tricks.com/almanac/properties/s/scrollbar/
 // claymophism https://hype4.academy/tools/claymorphism-generator
+
+export default function Detail() {
+ return <></>;
+}
